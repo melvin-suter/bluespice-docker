a4c3584e26dacb0be1e55f0cb760ff96426d81e8
Melvin Suter <melvin@suter.dev>
2023-07-12 15:47:02 +0200

    +

--- bluespice-web/scripts/entrypoint.sh
+++ bluespice-web/scripts/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Copy if not existing
-find /opt/bluespice_orig/settings.d/  -maxdepth 1 -type d | tail -n+2 | sed "s;/opt/bluespice_orig/;;" | while read line ; do   test -d /opt/bluespice/$line || cp -R /opt/bluespice_orig/$line /opt/bluespice/$line; done
+find /opt/bluespice_original//settings.d/  -maxdepth 1 -type d | tail -n+2 | sed "s;/opt/bluespice_original//;;" | while read line ; do   test -d /opt/bluespice/$line || cp -R /opt/bluespice_original//$line /opt/bluespice/$line; done
 
 # Copy all
-/bin/mv -f /opt/bluespice_orig/extensions/* /opt/bluespice/extensions
+/bin/mv -f /opt/bluespice_original//extensions/* /opt/bluespice/extensions
 
 docker-php-entrypoint --user 1000:1000
