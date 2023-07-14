a46b6bbfeacb2936ca0f4051a7bd9fd17b17564f
Melvin Suter <melvin@suter.dev>
2023-07-14 15:29:59 +0200

    +

--- bluespice-web/scripts/entrypoint.sh
+++ bluespice-web/scripts/entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+SECRET_KEY="$(hexdump -vn16 -e'4/4 "%08x" 2 "\n"' /dev/urandom)$(hexdump -vn16 -e'4/4 "%08x" 2 "\n"' /dev/urandom)"
+
+# For Testing create dirs
+test -d /images || mkdir -p /images
+test -d /settings || mkdir -p /settings/settings.d
+test -d /extensions || mkdir -p /extensions
+
 # Copy if not existing
-find /opt/bluespice_original/settings.d/  -maxdepth 1 -type d | tail -n+2 | sed "s;/opt/bluespice_original/;;" | while read line ; do   test -d /opt/bluespice/$line || cp -R /opt/bluespice_original/$line /opt/bluespice/$line; done
+find /var/www/html_original/settings.d/  -maxdepth 1 -type d | tail -n+2 | sed "s;/var/www/html_original/;;" | while read line ; do   test -d /var/www/html/$line || cp -R /var/www/html_original/$line /var/www/html/$line; done
 
 # Copy all
-test -d /opt/bluespice/extensions || mkdir /opt/bluespice/extensions
-/bin/mv -f /opt/bluespice_original/extensions/* /opt/bluespice/extensions
+test -d /var/www/html/extensions || mkdir /var/www/html/extensions
+/bin/mv -f /var/www/html_original/extensions/* /var/www/html/extensions
+
+# Create LocalSettings.php if needed
+test -f /settings/LocalSettings.php || cp /var/www/html_original/LocalSettings.php /settings/LocalSettings.php
+
+# Replace Vars
+sed -i "s;##BLUESPICE_SITE_NAME##;$BLUESPICE_SITE_NAME;" /var/www/html/LocalSettings.php
+sed -i "s;##BLUESPICE_URL##;$BLUESPICE_URL;" /var/www/html/LocalSettings.php
+sed -i "s;##MYSQL_USER##;$MYSQL_USER;" /var/www/html/LocalSettings.php
+sed -i "s;##MYSQL_PASSWORD##;$MYSQL_PASSWORD;" /var/www/html/LocalSettings.php
+sed -i "s;##MYSQL_DATABASE##;$MYSQL_DATABASE;" /var/www/html/LocalSettings.php
+sed -i "s;##BLUESPICE_TIMEZONE##;$BLUESPICE_TIMEZONE;" /var/www/html/LocalSettings.php
+sed -i "s;##BLUESPICE_LANGUAGE##;$BLUESPICE_LANGUAGE;" /var/www/html/LocalSettings.php
+sed -i "s;##SECRET_KEY##;$SECRET_KEY;" /var/www/html/LocalSettings.php
 
 docker-php-entrypoint
 apache2-foreground
