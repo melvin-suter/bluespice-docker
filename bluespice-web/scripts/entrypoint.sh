a56465c921b5173ce3603d171beb433bf1b79260
Melvin mngmt-02 <melvin@suter.dev>
2023-07-20 22:09:33 +0200

    fixing entry

--- bluespice-web/scripts/entrypoint.sh
+++ bluespice-web/scripts/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SECRET_KEY="$(hexdump -vn16 -e'4/4 "%08x" 2 "\n"' /dev/urandom)$(hexdump -vn16 -e'4/4 "%08x" 2 "\n"' /dev/urandom)"
+LOCALSETTINGS_TARGET="/var/www/html/LocalSettings.php"
 
 # For Testing create dirs
 test -d /images || mkdir -p /images
@@ -14,27 +15,38 @@ find /var/www/html_original/settings.d/  -maxdepth 1 -type d | tail -n+2 | sed "
 test -d /var/www/html/extensions || mkdir /var/www/html/extensions
 /bin/mv -f /var/www/html_original/extensions/* /var/www/html/extensions
 
-# Create LocalSettings.php if needed
-test -f /settings/LocalSettings.php || cp /opt/bluespice/LocalSettings.php /settings/LocalSettings.php
+# Run Installer if needed
+
+if [ -f $LOCALSETTINGS_TARGET ] ; then
+    [ -n "$MARIADB_USER" ] && php maintenance/install.php --conf $LOCALSETTINGS_TARGET "$BLUESPICE_SITE_NAME" "$BLUESPICE_INITIAL_ADMIN_NAME" --pass "$BLUESPICE_INITIAL_ADMIN_PASSWORD" --dbname "$MARIADB_DATABASE" --dbpass "$MARIADB_PASSWORD" --dbserver "$MARIADB_HOST" --dbuser "$MARIADB_USER"
+    [ -n "$MARIA_USER" ] && php maintenance/install.php --conf $LOCALSETTINGS_TARGET "$BLUESPICE_SITE_NAME" "$BLUESPICE_INITIAL_ADMIN_NAME" --pass "$BLUESPICE_INITIAL_ADMIN_PASSWORD" --dbname "$MARIA_DATABASE" --dbpass "$MARIA_PASSWORD" --dbserver "$MARIA_HOST" --dbuser "$MARIA_USER"
+    [ -n "$MYSQL_USER" ] && php maintenance/install.php --conf $LOCALSETTINGS_TARGET "$BLUESPICE_SITE_NAME" "$BLUESPICE_INITIAL_ADMIN_NAME" --pass "$BLUESPICE_INITIAL_ADMIN_PASSWORD" --dbname "$MYSQL_DATABASE" --dbpass "$MYSQL_PASSWORD" --dbserver "$MYSQL_HOST" --dbuser "$MYSQL_USER"
+
+    # Create LocalSettings.php
+    /bin/cp -f /opt/bluespice/LocalSettings.php /settings/LocalSettings.php
+
+fi
 
 # Replace Vars
-[ -n "$BLUESPICE_SITE_NAME" ] && sed -i "s;##BLUESPICE_SITE_NAME##;$BLUESPICE_SITE_NAME;" /var/www/html/LocalSettings.php
-[ -n "$BLUESPICE_URL" ] && sed -i "s;##BLUESPICE_URL##;$BLUESPICE_URL;" /var/www/html/LocalSettings.php
-[ -n "$MARIADB_HOST" ] && sed -i "s;##MYSQL_HOST##;$MARIADB_HOST;" /var/www/html/LocalSettings.php
-[ -n "$MARIADB_USER" ] && sed -i "s;##MYSQL_USER##;$MARIADB_USER;" /var/www/html/LocalSettings.php
-[ -n "$MARIADB_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MARIADB_PASSWORD;" /var/www/html/LocalSettings.php
-[ -n "$MARIADB_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MARIADB_DATABASE;" /var/www/html/LocalSettings.php
-[ -n "$MARIA_HOST" ] && sed -i "s;##MYSQL_HOST##;$MARIA_HOST;" /var/www/html/LocalSettings.php
-[ -n "$MARIA_USER" ] && sed -i "s;##MYSQL_USER##;$MARIA_USER;" /var/www/html/LocalSettings.php
-[ -n "$MARIA_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MARIA_PASSWORD;" /var/www/html/LocalSettings.php
-[ -n "$MARIA_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MARIA_DATABASE;" /var/www/html/LocalSettings.php
-[ -n "$MYSQL_HOST" ] && sed -i "s;##MYSQL_HOST##;$MYSQL_HOST;" /var/www/html/LocalSettings.php
-[ -n "$MYSQL_USER" ] && sed -i "s;##MYSQL_USER##;$MYSQL_USER;" /var/www/html/LocalSettings.php
-[ -n "$MYSQL_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MYSQL_PASSWORD;" /var/www/html/LocalSettings.php
-[ -n "$MYSQL_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MYSQL_DATABASE;" /var/www/html/LocalSettings.php
-[ -n "$BLUESPICE_TIMEZONE" ] && sed -i "s;##BLUESPICE_TIMEZONE##;$BLUESPICE_TIMEZONE;" /var/www/html/LocalSettings.php
-[ -n "$BLUESPICE_LANGUAGE" ] && sed -i "s;##BLUESPICE_LANGUAGE##;$BLUESPICE_LANGUAGE;" /var/www/html/LocalSettings.php
-[ -n "$SECRET_KEY" ] && sed -i "s;##SECRET_KEY##;$SECRET_KEY;" /var/www/html/LocalSettings.php
+[ -n "$BLUESPICE_SITE_NAME" ] && sed -i "s;##BLUESPICE_SITE_NAME##;$BLUESPICE_SITE_NAME;" $LOCALSETTINGS_TARGET
+[ -n "$BLUESPICE_URL" ] && sed -i "s;##BLUESPICE_URL##;$BLUESPICE_URL;" $LOCALSETTINGS_TARGET
+[ -n "$MARIADB_HOST" ] && sed -i "s;##MYSQL_HOST##;$MARIADB_HOST;" $LOCALSETTINGS_TARGET
+[ -n "$MARIADB_USER" ] && sed -i "s;##MYSQL_USER##;$MARIADB_USER;" $LOCALSETTINGS_TARGET
+[ -n "$MARIADB_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MARIADB_PASSWORD;" $LOCALSETTINGS_TARGET
+[ -n "$MARIADB_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MARIADB_DATABASE;" $LOCALSETTINGS_TARGET
+[ -n "$MARIA_HOST" ] && sed -i "s;##MYSQL_HOST##;$MARIA_HOST;" $LOCALSETTINGS_TARGET
+[ -n "$MARIA_USER" ] && sed -i "s;##MYSQL_USER##;$MARIA_USER;" $LOCALSETTINGS_TARGET
+[ -n "$MARIA_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MARIA_PASSWORD;" $LOCALSETTINGS_TARGET
+[ -n "$MARIA_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MARIA_DATABASE;" $LOCALSETTINGS_TARGET
+[ -n "$MYSQL_HOST" ] && sed -i "s;##MYSQL_HOST##;$MYSQL_HOST;" $LOCALSETTINGS_TARGET
+[ -n "$MYSQL_USER" ] && sed -i "s;##MYSQL_USER##;$MYSQL_USER;" $LOCALSETTINGS_TARGET
+[ -n "$MYSQL_PASSWORD" ] && sed -i "s;##MYSQL_PASSWORD##;$MYSQL_PASSWORD;" $LOCALSETTINGS_TARGET
+[ -n "$MYSQL_DATABASE" ] && sed -i "s;##MYSQL_DATABASE##;$MYSQL_DATABASE;" $LOCALSETTINGS_TARGET
+[ -n "$BLUESPICE_TIMEZONE" ] && sed -i "s;##BLUESPICE_TIMEZONE##;$BLUESPICE_TIMEZONE;" $LOCALSETTINGS_TARGET
+[ -n "$BLUESPICE_LANGUAGE" ] && sed -i "s;##BLUESPICE_LANGUAGE##;$BLUESPICE_LANGUAGE;" $LOCALSETTINGS_TARGET
+[ -n "$SECRET_KEY" ] && sed -i "s;##SECRET_KEY##;$SECRET_KEY;" $LOCALSETTINGS_TARGET
+
+
 
 docker-php-entrypoint
 apache2-foreground
